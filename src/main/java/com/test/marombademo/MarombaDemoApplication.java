31fda92565f4285a71951e7cad86c03b8ca59d4e
Vinicius <vinicius3.14ipu@gmail.com>
2023-07-04 22:47:20 -0300

    initial

---
+++ src/main/java/com/test/marombademo/MarombaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.test.marombademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarombaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarombaDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/marombademo/MarombaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.marombademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarombaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
